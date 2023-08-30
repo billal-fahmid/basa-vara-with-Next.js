ded4065c787f0cd82c3d8baabda86d74e7f43789
billal-fahmid <billalcooom@gmail.com>
2023-08-30 19:44:04 +0600

    update

---
+++ src/app/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBk5sXV-QGAaMw7Gm0O9lkWxV_8KzbD3w",
+  authDomain: "basa-vara-b3e1e.firebaseapp.com",
+  projectId: "basa-vara-b3e1e",
+  storageBucket: "basa-vara-b3e1e.appspot.com",
+  messagingSenderId: "1003697409974",
+  appId: "1:1003697409974:web:741f1c9e5c95f8ea09bb17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
